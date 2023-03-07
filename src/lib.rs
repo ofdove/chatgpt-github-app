96a7c2e9bf9cfbe4e80504edfa35eb11ce66fe6f
Trip Chien <65027448+ofdove@users.noreply.github.com>
2023-03-07 13:20:06 +0800

    Changed for my own parameters

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use openai_flows::chat_completion;
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
     listen_to_event(
-        "second-state",
+        "ofdove",
         "chat-with-chatgpt",
         vec!["issue_comment", "issues"],
         handler,
@@ -15,15 +15,15 @@ pub async fn run() {
 }
 
 async fn handler(payload: EventPayload) {
-    let octo = get_octo(Some(String::from("second-state")));
-    let issues = octo.issues("second-state", "chat-with-chatgpt");
+    let octo = get_octo(Some(String::from("ofdove")));
+    let issues = octo.issues("ofdove", "chat-with-chatgpt");
 
     match payload {
         EventPayload::IssueCommentEvent(e) => {
             if e.comment.user.r#type != "Bot" {
                 if let Some(b) = e.comment.body {
                     if let Some(r) =
-                        chat_completion("chatmichael", &format!("issue#{}", e.issue.number), &b)
+                        chat_completion("chatTrip", &format!("issue#{}", e.issue.number), &b)
                     {
                         if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
                             write_error_log!(e.to_string());
@@ -37,7 +37,7 @@ async fn handler(payload: EventPayload) {
             let title = e.issue.title;
             let body = e.issue.body.unwrap_or("".to_string());
             let q = title + "\n" + &body;
-            if let Some(r) = chat_completion("chatmichael", &format!("issue#{}", e.issue.number), &q) {
+            if let Some(r) = chat_completion("chatTrip", &format!("issue#{}", e.issue.number), &q) {
                 if let Err(e) = issues.create_comment(e.issue.number, r.choice).await {
                     write_error_log!(e.to_string());
                 }
